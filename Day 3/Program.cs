fa19520bee7a13a75aa17851c54102a62acc3cd6
Pointer Guide <pointerguide@gmail.com>
2022-12-03 20:45:05 +0100

    refactor day 3

--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -4,11 +4,11 @@ List<(char[], char[])> compartments = InputParser.Parse("input.txt").ToList();
 
 // Part one //
 IEnumerable<char> intesections = compartments.Select(c => c.Item1.Intersect(c.Item2).Single());
-List<int> priorities = intesections.Select(i => char.IsLower(i) ? i - 96 : i - 38).ToList();
+IEnumerable<int> priorities = intesections.Select(i => char.IsLower(i) ? i - 96 : i - 38);
 Console.WriteLine(priorities.Sum());
 
 // Part two //
 IEnumerable<HashSet<char>[]> groups = compartments.Select(c => c.Item1.Concat(c.Item2).ToHashSet()).Chunk(3);
 intesections = groups.Select(g => g[0].Intersect(g[1]).Intersect(g[2]).Single());
-priorities = intesections.Select(i => char.IsLower(i) ? i - 96 : i - 38).ToList();
+priorities = intesections.Select(i => char.IsLower(i) ? i - 96 : i - 38);
 Console.WriteLine(priorities.Sum());
