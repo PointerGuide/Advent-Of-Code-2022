157323285a8203aa9dfe09af3da1279cbae52366
Pointer Guide <pointerguide@gmail.com>
2022-12-03 20:29:00 +0100

    Refactor day 3

--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -3,26 +3,12 @@
 List<(char[], char[])> compartments = InputParser.Parse("input.txt").ToList();
 
 // Part one //
-List<int> priorities = new List<int>(compartments.Count);
-foreach ((char[], char[]) compartment in compartments)
-{
-    char intersection = compartment.Item1.Intersect(compartment.Item2).First();
-    
-    int priority = char.IsLower(intersection) ? intersection - 96 : intersection - 38; //convert ascii value to priority
-    priorities.Add(priority); 
-}
-
+IEnumerable<char> intesections = compartments.Select(c => c.Item1.Intersect(c.Item2).Single());
+List<int> priorities = intesections.Select(i => char.IsLower(i) ? i - 96 : i - 38).ToList();
 Console.WriteLine(priorities.Sum());
 
 // Part two //
 IEnumerable<HashSet<char>[]> groups = compartments.Select(c => c.Item1.Concat(c.Item2).ToHashSet()).Chunk(3);
-priorities.Clear();
-
-foreach (HashSet<char>[] group in groups)
-{
-    char intersection = group[0].Intersect(group[1]).Intersect(group[2]).First();
-    int priority = char.IsLower(intersection) ? intersection - 96 : intersection - 38; //convert ascii value to priority
-    priorities.Add(priority);
-}
-
+intesections = groups.Select(g => g[0].Intersect(g[1]).Intersect(g[2]).Single());
+priorities = intesections.Select(i => char.IsLower(i) ? i - 96 : i - 38).ToList();
 Console.WriteLine(priorities.Sum());
