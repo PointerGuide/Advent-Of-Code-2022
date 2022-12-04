bdd9e9781db0e34730d328ffe9cb1b688a1dd93c
Pointer Guide <pointerguide@gmail.com>
2022-12-04 12:19:10 +0100

    Day 4

--- Day 4/InputParser.cs
+++ Day 4/InputParser.cs
@@ -2,9 +2,17 @@
 {
     internal static class InputParser
     {
-        public static void Parse(string filename)
+        public static IEnumerable<(HashSet<int>, HashSet<int>)> Parse(string filename)
         {
             string[] lines = File.ReadAllLines(filename);
+            IEnumerable<(HashSet<int>, HashSet<int>)> sections =
+                lines.Select(l => l.Split(','))
+                     .Select(s => (s[0].Split('-'), s[1].Split('-')))
+                     .Select(s => (s.Item1.Select(i => int.Parse(i)).ToArray(),
+                                     s.Item2.Select(i => int.Parse(i)).ToArray()))
+                     .Select(s => (Enumerable.Range(s.Item1[0], s.Item1[1] - s.Item1[0] + 1).ToHashSet(),
+                                     Enumerable.Range(s.Item2[0], s.Item2[1] - s.Item2[0] + 1).ToHashSet()));
+            return sections;
         }
     }
 }
--- Day 4/Program.cs
+++ Day 4/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Day_4;
+
+// Part one //
+IEnumerable<(HashSet<int>, HashSet<int>)> sections = InputParser.Parse("input.txt");
+var fullyContains = sections.Select(s => s.Item1.IsSupersetOf(s.Item2) || s.Item1.IsSubsetOf(s.Item2));
+Console.WriteLine(fullyContains.Count(s => s));
+
+// Part two //
+var overlapPair = sections.Select(s => s.Item1.Overlaps(s.Item2));
+Console.WriteLine(overlapPair.Count(s => s));
