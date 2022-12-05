96e7df5d442e9bc91bfe9c9e4c6b9341aaae74f9
Pointer Guide <pointerguide@gmail.com>
2022-12-05 13:08:45 +0100

    prepare day 5

---
+++ Day 5/InputParser.cs
@@ -0,0 +1,11 @@
+﻿namespace Day_5
+{
+    internal static class InputParser
+    {
+        public static void Parse(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename);
+            
+        }
+    }
+}
