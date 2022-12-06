76dea3f790cfaa537a0f92ad53deda19db0dd98b
Pointer Guide <pointerguide@gmail.com>
2022-12-06 14:37:00 +0100

    day 6

---
+++ Day 6/Program.cs
@@ -0,0 +1,17 @@
+﻿char[] code = File.ReadAllText("input.txt").ToCharArray();
+
+// Part one //
+Console.WriteLine(FindStartOfPackedIndex(code, 4));
+
+// Part two //
+Console.WriteLine(FindStartOfPackedIndex(code, 14));
+
+int FindStartOfPackedIndex(char[] code, int packetLength)
+{
+    for (int idx = 0; idx < code.Length - packetLength; idx++)
+    {
+        if (code[new Range(idx, idx + packetLength)].ToHashSet().Count != packetLength) continue;
+        return idx + packetLength;
+    }
+    return -1;
+}
