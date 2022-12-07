391b9a98eaa15e41bfd9bb6e1017d5d9e5909280
Pointer Guide <pointerguide@gmail.com>
2022-12-07 18:12:13 +0100

    Prepare day 8

---
+++ Day 8/InputParser.cs
@@ -0,0 +1,10 @@
+﻿namespace Day_7
+{
+    internal static class InputParser
+    {
+        public static void Parse(string filename)
+        {
+            
+        }
+    }
+}
---
+++ Day 8/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine("Waiting so long :))))");
