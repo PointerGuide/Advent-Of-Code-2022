52fac1f57c7a86547866660ee758de7e74ca24c2
Pointer Guide <pointerguide@gmail.com>
2022-12-10 19:20:57 +0100

    day 10

--- Day 10/Program.cs
+++ Day 10/Program.cs
@@ -1,18 +1,37 @@
 ﻿using Day_10;
 
-List<(string instruction, int value)> commands = InputParser.Parse("example.txt");
-commands.ForEach(Execute);
+List<(string instruction, int value)> commands = InputParser.Parse("input.txt");
 int register = 1;
 int cycle = 1;
+int printed = 0;
+const int crtWidht = 40, crtHeight = 6;
+// Part one + two//
+List<int> signalStrengths = new List<int>();
+commands.ForEach(Execute);
+int signalTotalStrength = signalStrengths.Where((e, i) => new List<int> { 20, 60, 100, 140, 180, 220 }.Contains((i+1) * 20)).Sum();
+Console.WriteLine(signalTotalStrength);
 
 void Execute((string instruction, int value) command)
 {
-    int executionCycles = 1;
-    switch (command.instruction)
-    {
-        case "noop":
-            break;
-        case "addx":
-            break;
-    }
+    Cycle();
+    if (command.instruction == "noop") return;
+    register += command.value;
+    Cycle();
+}
+void Cycle()
+{
+    if (cycle % 20 == 0)
+        signalStrengths.Add(cycle*register);
+    DrawCrtPixel();
+    cycle++;
+    Thread.Sleep(1000/60);
+}
+void DrawCrtPixel()
+{
+    int diff = Math.Abs(register - cycle % 40);
+    char c = diff <= 1 ? '#' : '.';
+    Console.Write(c);
+    printed++;
+    if (cycle % 40 == 0)
+        Console.WriteLine();
 }
