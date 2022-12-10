e4a028603c9cf5812b13348f0c4d3bc80217d03c
Pointer Guide <pointerguide@gmail.com>
2022-12-10 16:22:36 +0100

    Rename and init day 10

---
+++ Day 10/InputParser.cs
@@ -0,0 +1,17 @@
+﻿namespace Day_10;
+
+internal static class InputParser
+{
+    public static List<(string instruction, int value)> Parse(string filename)
+    {
+        string[] lines = File.ReadAllLines(filename);
+        List<(string instruction, int value)> commands = new List<(string, int)>();
+        foreach (string line in lines)
+        {
+            string[] split = line.Split(' ');
+            int val = split.Length > 1 ? int.Parse(split[1]) : 0;
+            commands.Add((split[0], val));
+        }
+        return commands;
+    }
+}
---
+++ Day 10/Program.cs
@@ -0,0 +1,18 @@
+﻿using Day_10;
+
+List<(string instruction, int value)> commands = InputParser.Parse("example.txt");
+commands.ForEach(Execute);
+int register = 1;
+int cycle = 1;
+
+void Execute((string instruction, int value) command)
+{
+    int executionCycles = 1;
+    switch (command.instruction)
+    {
+        case "noop":
+            break;
+        case "addx":
+            break;
+    }
+}
